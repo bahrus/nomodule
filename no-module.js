7c5d6c8c8f29aebf1df9a1d69930c153978157e6
Bruce Anderson <anderson.bruce.b@gmail.com>
2020-07-04 18:13:27 -0400

    ...

--- no-module.js
+++ no-module.js
@@ -4,6 +4,7 @@ cssObserve.observe = true;
 cssObserve.selector = 'script[nomodule]';
 cssObserve.addEventListener('latest-match-changed', e => {
     e.detail.value.dataset.found = 'true';
+    loadScript(e.detail.value);
 });
 cssObserve.customStyles = /* css */ `
     script[nomodule]{
@@ -14,3 +15,20 @@ cssObserve.customStyles = /* css */ `
     }
 `;
 document.head.appendChild(cssObserve);
+function loadScript(scriptElement) {
+    const key = (new Date()).valueOf().toString();
+    window[key] = scriptElement;
+    const innerText = scriptElement.innerText;
+    const splitText = innerText.split('export const ');
+    let iPos = 0;
+    const winKey = `window['${key}']`;
+    for (let i = 1, ii = splitText.length; i < ii; i++) {
+        const token = splitText[i];
+        const iPosOfEq = token.indexOf('=');
+        const lhs = token.substr(0, iPosOfEq).trim();
+        splitText[i] = `const ${lhs}  = ${winKey}.${lhs} = ${token.substr(iPosOfEq + 1)};`;
+    }
+    const modifiedText = splitText.join('');
+    eval(modifiedText);
+    delete window[key];
+}
--- no-module.ts
+++ no-module.ts
@@ -5,6 +5,7 @@ cssObserve.observe = true;
 cssObserve.selector = 'script[nomodule]';
 cssObserve.addEventListener('latest-match-changed', e => {
     e.detail.value.dataset.found = 'true';
+    loadScript(e.detail.value);
 });
 cssObserve.customStyles = /* css */`
     script[nomodule]{
@@ -15,3 +16,21 @@ cssObserve.customStyles = /* css */`
     }
 `;
 document.head.appendChild(cssObserve);
+
+function loadScript(scriptElement: HTMLScriptElement){
+    const key = (new Date()).valueOf().toString();
+    (<any>window)[key] = scriptElement;
+    const innerText = scriptElement.innerText;
+    const splitText = innerText.split('export const ');
+    let iPos = 0;
+    const winKey = `window['${key}']`;
+    for(let i = 1, ii = splitText.length; i < ii; i++){
+        const token = splitText[i];
+        const iPosOfEq = token.indexOf('=');
+        const lhs = token.substr(0, iPosOfEq).trim();
+        splitText[i] = `const ${lhs}  = ${winKey}.${lhs} = ${token.substr(iPosOfEq + 1)};`
+    }
+    const modifiedText = splitText.join('');
+    eval(modifiedText);
+    delete (<any>window)[key];
+}
